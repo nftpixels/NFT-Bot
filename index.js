9e65e53cb8e6c649209d7cb10d24c6fb8064972f
nftpixels <97366705+nftpixels@users.noreply.github.com>
2024-02-02 14:46:34 +1300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Your Discord application details
 const clientId = '1202779676733607976';
 const clientSecret = 'Ft1bgEwnNucwMJpPhRBcqpMAJ4I-cQW4';
-const redirectUri = 'https://discord-bot-test-mq3a.onrender.com';
+const redirectUri = 'https://discord-bot-test-mq3a.onrender.com/Authentication';
 
 // Route for initiating the Discord OAuth2 flow
 app.get('/connect', (req, res) => {
